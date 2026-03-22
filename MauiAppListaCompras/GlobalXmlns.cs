6aaea9caa15f0be1970fa1061fdfdf4681de500a
Alexandre210183 <alexandre21santosamorim@gmail.com>
2026-03-22 14:54:53 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppListaCompras/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppListaCompras")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppListaCompras.Pages")]
